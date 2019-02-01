a9d750e2f9ad26cf47b4df27cafa22039c3afb5b
camila_fiaminghi <camila.fiaminghi@gmail.com>
2019-02-01 18:49:32 -0200

    Test component Question

---
+++ components/__tests__/Question.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { TouchableOpacity } from 'react-native'
+import renderer from 'react-test-renderer'
+import Question from '../Question'
+import card from '../../__helpers__/card'
+
+let wrapper
+let instance
+let component
+const props = {
+	question: card.question,
+	toggleSide: jest.fn()
+}
+
+describe('<Question />', () => {
+
+	beforeEach(() => {
+		component = shallow(<Question {...props} />)
+	})
+
+	it('matches snapshot', () => {
+    wrapper = renderer.create(<Question {...props} />)
+    expect(wrapper.toJSON()).toMatchSnapshot()
+  });
+
+	it('should render', () => {
+		expect(wrapper).toBeTruthy()
+	})
+
+	it('should call toggleSide onPress TouchableOpacity', () => {
+		const button = component.find(TouchableOpacity).get(0)
+		button.props.onPress()
+
+		expect(props.toggleSide).toHaveBeenCalledTimes(1)
+	})
+})
